d60dca2a365cfb51cd5de88ad18010d36bbb1828
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-16 10:55:42 +0100

    Resolved bugs in Osiris

--- code/Osiris.py
+++ code/Osiris.py
@@ -45,56 +45,40 @@ for article in root.findall("Article"):
         if children.tail is not None:
             titleText = titleText + children.tail
 
-    # Minor error analysis
-    for annotation in annotations:
-        if annotation["text"] != titleText[annotation["begin"]:annotation["end"]]:
-            print(annotation["text"])
-
-            print(titleText[annotation["begin"]:annotation["end"]])
-            print("---")
-
-
 
     # 2.) Handle abstract
-    abstractText = ""
+    fulltext = titleText +"\n"
+    offset = len(fulltext)
     if abstractElement.text is not None:
-        abstractText = abstractElement.text
+        fulltext = fulltext + abstractElement.text
 
     annotations = []
 
     for children in list(abstractElement):
         if children.tag == "gene":
-            annotations.append({"ID": "T" + str(len(annotations)), "type": children.tag, "begin": len(abstractText), "end": len(abstractText) + len(children.text),
+            annotations.append({"ID": "T" + str(len(annotations)), "type": children.tag, "begin": len(fulltext), "end": len(fulltext) + len(children.text),
              "text": children.text, "entrez": children.get("g_id"), })
 
         elif children.tag == "variant":
             if children.get("v_id").isnumeric():  # Otherwise entity cannot be normalized to dbSNP
-                annotations.append({"ID": "T" + str(len(annotations)), "type": children.tag, "begin": len(abstractText),
-                                "end": len(abstractText) + len(children.text),
+                annotations.append({"ID": "T" + str(len(annotations)), "type": children.tag, "begin": len(fulltext),
+                                "end": len(fulltext) + len(children.text),
                                 "text": children.text, "dbSNP": children.get("v_id") })
             else:
-                annotations.append({"ID": "T" + str(len(annotations)), "type": children.tag, "begin": len(abstractText),
-                                "end": len(abstractText) + len(children.text),
+                annotations.append({"ID": "T" + str(len(annotations)), "type": children.tag, "begin": len(fulltext),
+                                "end": len(fulltext) + len(children.text),
                                 "text": children.text })
 
         else:
             print("Error no handling for '" +children.tag +"'")
 
-        abstractText = abstractText +children.text
+        fulltext = fulltext +children.text
         if children.tail is not None:
-            abstractText = abstractText + children.tail
-
-    # Minor error analysis
-    for annotation in annotations:
-        if annotation["text"] != abstractText[annotation["begin"]:annotation["end"]]:
-            print(annotation["text"])
-
-            print(abstractText[annotation["begin"]:annotation["end"]])
-            print("---")
+            fulltext = fulltext + children.tail
 
     jsonDocument = {"document": {
         "ID": pmid,
-        "text": text,
+        "text": fulltext,
         "entities": annotations,
         "relations": [],
         "metadata": []
