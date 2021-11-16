035f9dede87c601b4a47f539425acf4522f38ec5
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-16 10:12:56 +0100

    Resolved errors in thomas corpus

--- code/thomas2011.py
+++ code/thomas2011.py
@@ -20,7 +20,7 @@ for line in corpusFile:
     if array[0] not in annotationDict:
         annotationDict[array[0]] = []
 
-    annotationDict[array[0]].append({"ID": "T" + str(len(annotationDict[array[0]])),  "begin": int(array[3]) , "end": int(array[4]) , "text": array[1][1:-1], "dbSNP" : array[5], "type" : array[6]})
+    annotationDict[array[0]].append({"ID": "T" + str(len(annotationDict[array[0]])),  "begin": int(array[3]) , "end": int(array[4]) , "text": array[1][1:-1], "dbSNP" : int(array[5].split("rs")[1]), "type" : array[6]})
 corpusFile.close()
 
 #2.) Get the documents from NCBI eutils
@@ -46,20 +46,30 @@ else:
         pickle.dump(documentDict, fid)
     fid.close()
 
-
+errors = 0
+errorDocs = set()
 #3.) Build the documents for JSON serializaion
 jsonDocuments = []
 for pmid in annotationDict.keys():
 
     root = ET.fromstring(documentDict[pmid])
     title = root.find("PubmedArticle").find("MedlineCitation").find("Article").find("ArticleTitle").text
+    #We modify titles which are encapsulated in a [].
+    if title.startswith("[") and title.endswith("]."):
+        title = title[1:-2]
 
     abstr = ""
     for abstract in root.find("PubmedArticle").find("MedlineCitation").find("Article").find("Abstract").findall("AbstractText"):
+        #Add the label of an abstract section, if it is not the tag unlabelled
+        if "Label" in abstract.keys() and abstract.get("Label") != "UNLABELLED":
+            abstr +=  abstract.get("Label")
+            abstr += "  "
+
         abstr += (abstract.text)
         abstr += "\n"
 
     documentText = title +"\n\n" +abstr
+    #print(documentText)
 
     for annotation in annotationDict[pmid]:
         if annotation["text"] != documentText[annotation["begin"] : annotation["end"]]:
@@ -68,6 +78,8 @@ for pmid in annotationDict.keys():
             print(annotation)
             print(title +"\n" +abstr)
             print("----")
+            errors = errors +1
+            errorDocs.add(pmid)
 
     jsonDocument = {"document": {
         "ID": pmid,
@@ -85,6 +97,5 @@ f = open(outFile, "w")
 f.write(json.dumps(corpus, indent=4))
 f.close()
 
-response = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&rettype=xml&id="+"18201684")
-xml = response.text
-root = ET.fromstring(xml)
+#Print some errors
+print("No errors= " +str(errors) +" in " +str(len(errorDocs))  +" documents=" +str(errorDocs))
