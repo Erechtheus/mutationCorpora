9e2557cfe16bc2109b4c2aebd66303e152714ffa
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-22 12:02:17 +0100

    Reworked our code for evaluation

--- code/calculateNormalizationPerformance.py
+++ code/calculateNormalizationPerformance.py
@@ -35,9 +35,7 @@ snpDict = getSNPs(dbSNPIDs)
 
 
 
-tp = 0
-fp = 0
-fn = 0
+performances = {} #We save TP, FP, FN for each mutation-type in this variable
 #Iterate the goldstandard
 for goldDocument in goldDocuments["documents"]:
     documentID = goldDocument["document"]["ID"]
@@ -50,46 +48,63 @@ for goldDocument in goldDocuments["documents"]:
             goldRS = goldEntity["dbSNP"]        #The correct dbSNP-ID
             goldType = goldEntity["type"]       #The type of the mutation
 
+            if goldType not in  performances:
+                performances[goldType] = {}
+                performances[goldType]["tp"] = 0
+                performances[goldType]["fp"] = 0
+                performances[goldType]["fn"] = 0
+
             #Search the respective prediction doocument
             predDocument = list(filter(lambda x : x["ID"] == documentID, predDocuments))
             if len(predDocument) != 1:
-                print("SHould not happen! We found " +str(len(predDocument)) +" != 1 documents for " +str(documentID))
+                print("Should not happen! We found " +str(len(predDocument)) +" != 1 documents for " +str(documentID))
 
             else:
                 #Search the respective prediction for the entity
                 predEntity = list(filter(lambda x : x["ID"] == goldID, predDocument[0]["entities"] ))
                 if len(predEntity) > 1:
-                    print("SHould not happen! We received more than one entity with ID='" +str(goldID) +"'")
-                # If we find not the entity and if the entity has no rs-ID -> FN
+                    print("Should not happen! We received more than one entity with ID='" +str(goldID) +"'")
+
+                # If we find not the entity or if the returned entity has no rs-ID -> FN
                 elif len(predEntity) == 0 or len(predEntity[0]["rs"]) ==0:
-                    fn = fn +1
+                    performances[goldType]["fn"] = performances[goldType]["fn"] + 1
 
                 #In this case we have at least one prediction for the named entity
                 else:
-                    predictedRSs = predEntity[0]["rs"]
-
-                    tmpTP = False
-                    tmpFP = False
-                    tmpFN = False
+                    predictedRSs = set(predEntity[0]["rs"])
 
                     print(documentID +"\t" +goldID +"\trs=" +str(goldRS) +"\t" +str(predictedRSs))
 
+
                     if goldRS in predictedRSs:
-                        tp = tp +1
-                    if goldRS not in predictedRSs:
-                        fn = fn + 1
+                        performances[goldType]["tp"] = performances[goldType]["tp"] +1
+                        predictedRSs.remove(goldRS)
+                        print("TP")
+                    else:
+                        performances[goldType]["fn"] = performances[goldType]["fn"] +1
+                        print("FN")
+
+                    if len(predictedRSs) > 0:
+                        performances[goldType]["fp"] = performances[goldType]["fp"] +len(predictedRSs)
+                        print("FP")
 
 
-print("TP=" +str(tp))
-print("FP=" +str(fp))
-print("FN=" +str(fn))
+for key in performances.keys():
+    tp = performances[key]["tp"]
+    fp = performances[key]["fp"]
+    fn = performances[key]["fn"]
 
-precision = tp/(tp+fp)
-recall = tp/(tp+fn)
-f1  =  2*(recall * precision) / (recall + precision)
-print("Precision=%.2f" % precision )
-print("Recall=%.2f" %recall)
-print("F1=%.2f" %f1)
+    print("------" +key +"------")
+    print("TP=" +str(tp))
+    print("FP=" +str(fp))
+    print("FN=" +str(fn))
+
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    f1  =  2*(recall * precision) / (recall + precision)
+    print("Precision=%.2f" % precision )
+    print("Recall=%.2f" %recall)
+    print("F1=%.2f" %f1)
 
 
 
