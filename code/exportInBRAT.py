614359506ce0574e6beae32c1a1a0388bd85e22d
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-23 13:38:17 +0100

    Cleanup

--- code/exportInBRAT.py
+++ code/exportInBRAT.py
@@ -2,22 +2,6 @@ import json
 import os
 from pathlib import Path
 
-"""
-
-* start brat container:  
-```shell script
-docker run --name=brat -d -p 81:80 -v ~/.brat/data:/bratdata -v ~/.brat/config:/bratcfg -e BRAT_USERNAME=brat -e BRAT_PASSWORD=brat -e BRAT_EMAIL=brat@example.com cassj/brat
-```
-* update brat data with corpus:
-```shell script
-sudo rm -rf ~/.brat/data/corpus
-sudo cp -r corpora/BRAT/ ~/.brat/data/corpus
-sudo chmod -R 755 ~/.brat/data/corpus
-sudo chown -R www-data:www-data ~/.brat/data/corpus
-```
-
-"""
-
 inFolder="../corpora/json/"
 outFOlder="../corpora/BRAT/"
 
