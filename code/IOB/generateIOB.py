c8286ba92c182b7a742b74032a149c8a038f08d1
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-30 13:03:09 +0100

    Changed path

--- code/IOB/generateIOB.py
+++ code/IOB/generateIOB.py
@@ -3,14 +3,11 @@ import os
 from pathlib import Path
 import spacy # #python -m spacy download en_core_web_sm
 
-nlp = spacy.load('en_core_web_sm')
-
-
 randomSeed = 1202
 splitSize = 80 / 100
-inFolder="corpora/json/"
-splitFolder="code/IOB/splits/"
-iobFolder="code/IOB/IOB/"
+inFolder="../../corpora/json/"
+splitFolder="../../corpora/IOB/splits/"
+iobFolder="../../corpora/IOB/IOB/"
 
 if __name__ == "__main__":
     print("Generate IOB files using the train/test splits")
@@ -72,6 +69,7 @@ def generateIOB(document):
 
 
 
+nlp = spacy.load('en_core_web_sm')
 for path in Path(inFolder).glob('*.json'):
 
     folderName = os.path.basename(path)
