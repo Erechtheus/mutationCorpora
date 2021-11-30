84872591686f05dc6a83e2e57e2e2f1f89edc528
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-30 11:42:02 +0100

    First version of IOB

--- code/IOB/generateIOB.py
+++ code/IOB/generateIOB.py
@@ -12,77 +12,103 @@ inFolder="corpora/json/"
 splitFolder="code/IOB/splits/"
 iobFolder="code/IOB/IOB/"
 
-def is_overlapping(x1,x2,y1,y2):
-    return max(x1,y1) <= min(x2,y2)
-
 if __name__ == "__main__":
     print("Generate IOB files using the train/test splits")
 
-for path in Path(inFolder).glob('*.json'):
+def is_overlapping(x1,x2,y1,y2):
+    return max(x1,y1) <= min(x2,y2)
 
-    print(path)
+def writeStringToFile(fileName, string):
+    with open(fileName, "w") as text_file:
+        text_file.write(string)
+
+#Generate an IOB representation for a single JSON-document
+#Be warned this is extremely hacky code with cubic complexity
+#Someone, please implement this better
+def generateIOB(document):
+    iobString = ""
+
+    docID = document["document"]["ID"]
+    docText = document["document"]["text"]
+    docEntities = document["document"]["entities"]
+
+    iobString = iobString + "#" + docID + "\n"
+
+    doc = nlp(docText)
+    for sent in doc.sents:  # access sentences
+        tokens = []
+        for token in sent:  # access words/symbols (tokens)
+            tokens.append(token)
+        labels = ["O"] * len(tokens)
+
+        for entity in docEntities:
+            entityStart = entity["begin"]
+            entityEnd = entity["end"]
+            entityTpe = entity["type"]
+
+            markAsEntity = []
+            for tokenIndex in range(len(tokens)):
+                token = tokens[tokenIndex]
+                tokenStart = token.idx
+                tokenEnd = token.idx + len(token.text)
+
+                if is_overlapping(entityStart, entityEnd, tokenStart, tokenEnd):
+                    markAsEntity.append(tokenIndex)
+
+                for marker in range(len(markAsEntity)):
+                    markIndex = markAsEntity[marker]
+
+                    if marker == 0:
+                        labels[markIndex] = "B-" + entityTpe
+
+                    else:
+                        labels[markIndex] = "I-" + entityTpe
+
+        for tokenIndex in range(len(tokens)):
+            iobString = iobString + str(tokens[tokenIndex]) + "," + labels[tokenIndex] + "\n"
+        iobString = iobString + " , \n"  # next sentence
+
+    return iobString
+
+
+
+for path in Path(inFolder).glob('*.json'):
 
     folderName = os.path.basename(path)
     folderName = folderName[:-5]
-
-    #Skip corpora with dedicated train/test splits (e.g., AMIA or tmvar)
-    if "train" in folderName or "test" in folderName:
-        continue
-
-    with open(splitFolder +folderName +".json") as f:
-        splits = json.load(f)
-    f.close()
-
+    print(folderName)
 
     with open(path) as f:
         corpus = json.load(f)
     f.close()
 
-    iobString = "Word,Tag\n"
-    for document in corpus["documents"]:
-        docID = document["document"]["ID"]
-        docText = document["document"]["text"]
-        docEntities = document["document"]["entities"]
 
-        iobString = iobString +"#"  +docID +"\n"
+    #Skip corpora with dedicated train/test splits (e.g., AMIA or tmvar)
+    if "train" in folderName or "test" in folderName:
 
-        doc = nlp(docText)
-        for sent in doc.sents:  # access sentences
-            tokens = []
-            for token in sent:  # access words/symbols (tokens)
-                tokens.append(token)
-            labels = ["O"] * len(tokens)
+        iobString = "Word,Tag\n"
+        for document in corpus["documents"]:
+            iobString = iobString +generateIOB(document)
+        writeStringToFile(iobFolder +folderName +".iob", iobString)
 
-            for entity in docEntities:
-                entityStart = entity["begin"]
-                entityEnd = entity["end"]
-                entityTpe = entity["type"]
+    else:
 
-                markAsEntity = []
-                for tokenIndex in range(len(tokens)):
-                    token =tokens[tokenIndex]
-                    tokenStart = token.idx
-                    tokenEnd = token.idx + len(token.text)
+        with open(splitFolder +folderName +".json") as f:
+            splits = json.load(f)
+        f.close()
 
-                    if is_overlapping(entityStart, entityEnd, tokenStart, tokenEnd):
-                        markAsEntity.append(tokenIndex)
+        #Train-split
+        trainIOBString = "Word,Tag\n"
+        ids = set(splits["trainIDs"])
+        for document in list(filter(lambda document: document["document"]["ID"] in ids , corpus["documents"])):
+            trainIOBString = trainIOBString + generateIOB(document)
 
+        writeStringToFile(iobFolder +folderName +"-train.iob", trainIOBString)
 
-                    for marker in range(len(markAsEntity)):
-                        markIndex=markAsEntity[marker]
+        #Test-split
+        testIOBString = "Word,Tag\n"
+        ids = set(splits["testIDs"])
+        for document in list(filter(lambda document: document["document"]["ID"] in ids, corpus["documents"])):
+            testIOBString = testIOBString + generateIOB(document)
 
-                        if marker == 0:
-                            labels[markIndex] = "B-" +entityTpe
-
-                        else:
-                            labels[markIndex] = "I-" +entityTpe
-
-            for tokenIndex in range(len(tokens)):
-                iobString = iobString + str(tokens[tokenIndex]) +"," +labels[tokenIndex] +"\n"
-            iobString = iobString +" , \n"#next sentence
-
-    #print(iobString)
-
-
-def generateIOB(document, entities):
-    print("A")
+        writeStringToFile(iobFolder +folderName +"-test.iob", testIOBString)
