1dfecea449b4b6b2e192c21832e83bb3b81757d7
Philippe Thomas <philippe.thomas@dfki.de>
2021-11-30 11:07:50 +0100

    temporary commit

--- code/IOB/generateIOB.py
+++ code/IOB/generateIOB.py
@@ -8,16 +8,19 @@ nlp = spacy.load('en_core_web_sm')
 
 randomSeed = 1202
 splitSize = 80 / 100
-inFolder="../../corpora/json/"
-splitFolder="splits/"
-iobFolder="IOB"
+inFolder="corpora/json/"
+splitFolder="code/IOB/splits/"
+iobFolder="code/IOB/IOB/"
 
-
+def is_overlapping(x1,x2,y1,y2):
+    return max(x1,y1) <= min(x2,y2)
 
 if __name__ == "__main__":
     print("Generate IOB files using the train/test splits")
 
 for path in Path(inFolder).glob('*.json'):
+
+    print(path)
 
     folderName = os.path.basename(path)
     folderName = folderName[:-5]
@@ -25,8 +28,6 @@ for path in Path(inFolder).glob('*.json'):
     #Skip corpora with dedicated train/test splits (e.g., AMIA or tmvar)
     if "train" in folderName or "test" in folderName:
         continue
-
-
 
     with open(splitFolder +folderName +".json") as f:
         splits = json.load(f)
@@ -37,14 +38,51 @@ for path in Path(inFolder).glob('*.json'):
         corpus = json.load(f)
     f.close()
 
+    iobString = "Word,Tag\n"
     for document in corpus["documents"]:
         docID = document["document"]["ID"]
         docText = document["document"]["text"]
         docEntities = document["document"]["entities"]
 
-        doc = nlp(docText)
+        iobString = iobString +"#"  +docID +"\n"
 
+        doc = nlp(docText)
         for sent in doc.sents:  # access sentences
+            tokens = []
             for token in sent:  # access words/symbols (tokens)
-                print((token))
-                print(token.idx)
+                tokens.append(token)
+            labels = ["O"] * len(tokens)
+
+            for entity in docEntities:
+                entityStart = entity["begin"]
+                entityEnd = entity["end"]
+                entityTpe = entity["type"]
+
+                markAsEntity = []
+                for tokenIndex in range(len(tokens)):
+                    token =tokens[tokenIndex]
+                    tokenStart = token.idx
+                    tokenEnd = token.idx + len(token.text)
+
+                    if is_overlapping(entityStart, entityEnd, tokenStart, tokenEnd):
+                        markAsEntity.append(tokenIndex)
+
+
+                    for marker in range(len(markAsEntity)):
+                        markIndex=markAsEntity[marker]
+
+                        if marker == 0:
+                            labels[markIndex] = "B-" +entityTpe
+
+                        else:
+                            labels[markIndex] = "I-" +entityTpe
+
+            for tokenIndex in range(len(tokens)):
+                iobString = iobString + str(tokens[tokenIndex]) +"," +labels[tokenIndex] +"\n"
+            iobString = iobString +" , \n"#next sentence
+
+    #print(iobString)
+
+
+def generateIOB(document, entities):
+    print("A")
