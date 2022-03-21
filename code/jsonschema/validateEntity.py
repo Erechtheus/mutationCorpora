357f1534990f64213112e3314cc059025d03a75b
Philippe Thomas <philippe.thomas@dfki.de>
2022-03-21 15:19:25 +0100

    Working on JSON-validation

---
+++ code/jsonschema/validateEntity.py
@@ -0,0 +1,42 @@
+from jsonschema import validate
+import json
+
+f = open('code/jsonschema/entities.json')
+newSchema = json.load(f)
+f.close()
+
+#https://pypi.org/project/jsonschema2md/
+
+entities = [
+    {
+    "ID": "T4",
+    "type": "DNA_Mutation",
+    "begin": 1114,
+    "end": 1122,
+    "text": "mutation"
+    },
+    {
+        "ID": "T6",
+        "type": "DNA_Mutation",
+        "begin": 137,
+        "end": 146,
+        "text": "Mutations"
+    }
+]
+
+
+
+
+#We allow empty entities array
+validate(instance={"entities": []}, schema=newSchema)
+#We allow filled entities array
+validate(instance={"entities": entities}, schema=newSchema)
+
+#We dissalow missing text
+validate(instance = {"entities" : [{"ID": "T4", "type": "DNA_Mutation", "begin": 1114,"end": 1122,"ipsum": "mutation"}]}, schema=newSchema)
+
+#We dissalow wrong type (i.e. string instread of integer)
+validate(instance = {"entities" : [{"ID": "T4", "type": "DNA_Mutation", "begin": "1114","end": 1122,"text": "mutation"}]}, schema=newSchema)
+
+#We require entities as input
+validate(instance = {}, schema=newSchema)
